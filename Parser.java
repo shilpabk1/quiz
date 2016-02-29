c78e8bc69e4d5b7ee82547923c54efff5ac137f7
shilpabk1 <shilpa.rao.b@gmail.com>
2016-02-28 17:56:29 -0800

    Changes for Refractoring.
    1. Synchronize save and read methods to make the class thread safe.
    2. Used StringBuilder than String to improve runtime and memory efficiency
    3. Refactored to remove duplicate code.
    4. Give meaningful names for variables.

--- Parser.java
+++ Parser.java
@@ -2,41 +2,53 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.StringBuilder;
 /**
  * This class is thread safe.
  */
 public class Parser {
+  
   private File file;
-  public synchronized void setFile(File f) {
-    file = f;
+  
+  public synchronized void setFile(File sampleFile) {
+    file = sampleFile;
   }
+  
   public synchronized File getFile() {
     return file;
   }
+  
   public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
+    return readContent(false);
   }
+  
   public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
+    return readContent (true);
+  }
+  
+  private synchronized StringBuilder readContent(boolean withUniCode) {
+    FileInputStream inputStream = new FileInputStream(file);
+    StringBuilder output = "";
     int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
+     while ((data = inputStream.read()) > 0) {
+       if(withUniCode) {
+         if (data < 0x80) {
+            output.append((char) data);
+          }
+       } else {
+         output.append((char) data);
+       }
     }
+    inputStream.close();
     return output;
   }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
+  
+  public synchronized void saveContent(String content) throws IOException {
+    FileOutputStream outputStream = new FileOutputStream(file);
     for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+      outputStream.write(content.charAt(i));
     }
+    outputStream.close();
   }
+  
 }
